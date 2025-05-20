21bc9f6c3a8cc17d8c7d04377204ec433be100d3
William Morais Weidgenant <WilliamMorais.Weidgenant@philips.com>
2025-05-20 13:24:21 -0300

    inserindo javascript na página

---
+++ script.js
@@ -0,0 +1,56 @@
+function saudar() {
+    let nomeUsuario = prompt("Qual o seu nome?");
+    if (nomeUsuario == null || nomeUsuario == "") {
+        while(nomeUsuario == null || nomeUsuario == "")
+        nomeUsuario = prompt("Nome não informado! Informe seu nome");
+    }
+    alert("Olá, " + nomeUsuario + "!");
+}
+
+function validar() {
+  const nome     = document.getElementById("nome").value.trim();
+  const email    = document.getElementById("email").value.trim();
+  const telefone = document.getElementById("telefone").value.trim();
+  const motivo   = document.getElementById("motivo").value.trim();
+
+  console.log("validar() chamada!");
+  console.log({ nome, email, telefone, motivo });
+
+  if (!nome) {
+    alert("Preencha o nome.");
+    return false;
+  }
+  if (!email) {
+    alert("Preencha o e‑mail.");
+    return false;
+  }
+  if (!telefone) {
+    alert("Preencha o telefone.");
+    return false;
+  }
+  if (!motivo) {
+    alert("Preencha o motivo.");
+    return false;
+  }
+
+  alert("Formulário enviado com sucesso!"+
+    "\n nome: " + nome +
+    "\n email: " + email +
+    "\n telefone: " + telefone +
+    "\n motivo: " + motivo
+  );
+  return true;
+}
+
+const toggleBtn = document.getElementById('toggle-theme');
+const bodyEl = document.body;
+
+toggleBtn.addEventListener('click', () => {
+  if (bodyEl.classList.contains('theme-light')) {
+    bodyEl.classList.replace('theme-light', 'theme-dark');
+    toggleBtn.textContent = 'Tema Claro';
+  } else {
+    bodyEl.classList.replace('theme-dark', 'theme-light');
+    toggleBtn.textContent = 'Tema Escuro';
+  }
+});
